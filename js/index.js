ad31a199951a38217e9c8231b9195b17dcc34805
ruckus777 <52668955+ruckus777@users.noreply.github.com>
2019-08-08 10:58:28 -0400

    done

--- js/index.js
+++ js/index.js
@@ -1,1 +1,34 @@
 // Your code goes here //i started
+et destination = document.querySelectorAll('.destination')
+
+let callBack = e =>{
+    e.stopPropagation()
+    alert('clicked')
+}
+
+let stopProp = e =>{
+    e.stopPropagation()
+    alert('clicked Button')
+}
+
+destination[0].addEventListener('click',callBack)
+
+let dButton = destination[0].querySelector('.btn')
+console.log(dButton)
+dButton.addEventListener('click',stopProp)
+
+let navLinks = document.querySelectorAll('.nav .nav-link')
+navLinks.forEach(link =>{
+    link.addEventListener('mouseover', function(e){
+        link.style.color = "blue";
+    })
+    link.addEventListener('click', function(e){
+        e.preventDefault()
+        link.classList.toggle("mystyle");
+    })
+})
+
+let busImage = document.querySelector('.intro img')
+window.addEventListener('scroll', function(e){
+    busImage.classList.toggle("cat");
+})
